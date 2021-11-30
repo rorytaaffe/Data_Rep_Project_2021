4114a42f2d85038a731f280e0f5dd94f389f263c
rorytaaffe <G00332281@gmit.ie>
2021-11-30 15:52:24 +0000

    First Commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,6 +4,7 @@ const port = 4000
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(cors());
 
@@ -14,6 +15,9 @@ res.header("Access-Control-Allow-Headers",
 "Origin, X-Requested-With, Content-Type, Accept");
 next();
 });
+
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build/static')));
 
 
 // parse application/x-www-form-urlencoded
@@ -121,6 +125,10 @@ app.post('/api/movies', (req, res)=>{
     })
 
     res.send('Item Added')
+})
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname + '/../build/index.html'));
 })
 
 app.listen(port, () => {
